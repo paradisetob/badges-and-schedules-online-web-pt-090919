99c8eb522e7cdb15b4c61e80e08b3908d4f9cf00
tracey brimage <onelovenilynn@gmail.com>
2019-09-20 15:44:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+attendees="Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz")
+counter=1
+attendees.each do|attendees|
+   puts " batch_badge_creator #{ "Hello, my name is _____."}"
+   puts " assign_rooms #{counter}"
+   puts "printer #{batch_badge_creator,assign_rooms}"
+   counter += 1
+ end
