12ec07de23d4941f682e7691c2923392f51f3393
tracey brimage <onelovenilynn@gmail.com>
2019-09-20 14:56:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
 attendees="Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz")
 counter=1
 attendees.each do|attendees|
-   puts " batch_badge_creator #{attendees)."
-   puts ""
-   puts
+   puts " batch_badge_creator #{attendees)"
+   puts " assign_rooms #{counter}"
+   puts "printer #{}"
+ end
