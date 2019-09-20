d97a580954094fb7fc173728a4e9b1f226ddc01b
tracey brimage <onelovenilynn@gmail.com>
2019-09-20 15:34:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ counter=1
 attendees.each do|attendees|
    puts " batch_badge_creator #{ "Hello, my name is _____."}"
    puts " assign_rooms #{counter}"
-   puts "printer #{}"
+   puts "printer #{batch_badge_creator,assign_rooms}"
    counter += 1
  end
